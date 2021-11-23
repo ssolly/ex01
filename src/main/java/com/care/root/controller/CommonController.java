88fd3d034bbe616055450c2fffa9ce6b0a4941aa
unknown <solly0829@gmail.com>
2021-11-23 10:23:56 +0900

    package,class 경로 재설정

--- src/main/java/com/care/root/controller/CommonController.java
+++ src/main/java/com/care/root/controller/CommonController.java
@@ -1,4 +1,4 @@
-package com.care.root;
+package com.care.root.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
